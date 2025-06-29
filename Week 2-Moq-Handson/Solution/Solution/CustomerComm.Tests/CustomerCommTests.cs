cdc88afd8bf25ef0dc5c467c1cf3b63853dfd959
debarchita-123 <debarchita2020@gmail.com>
2025-06-30 01:49:17 +0530

    Add files via upload

---
+++ Week 2-Moq-Handson/Solution/Solution/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+[TestFixture]
+public class CustomerCommTests
+{
+    private Mock<IMailSender> _mockMailSender;
+    private CustomerComm _customerComm;
+
+    [OneTimeSetUp]
+    public void Init()
+    {
+        _mockMailSender = new Mock<IMailSender>();
+        _customerComm = new CustomerComm(_mockMailSender.Object);
+    }
+
+    [Test]
+    public void SendMailToCustomer_ShouldReturnTrue()
+    {
+        // Arrange
+        _mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+        // Act
+        var result = _customerComm.SendMailToCustomer();
+
+        // Assert
+        Assert.That(result);
+    }
+}
