338afaf500d157f191cc901508c317693c8a8c05
debarchita-123 <debarchita2020@gmail.com>
2025-07-21 00:23:10 +0530

    Add files via upload

---
+++ WEEK-5-ASP.NET Core 8.0 Web API/WEEK-5-ASP.NET Core 8.0 Web API/6. WebApi_Handson/Question-2/Code/KafkaConsumerApp/KafkaConsumerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-consumer-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("chat-topic");
+
+        Console.WriteLine("Kafka Consumer started. Listening for messages...");
+
+        try
+        {
+            while (true)
+            {
+                var cr = consumer.Consume();
+                Console.WriteLine($"Friend: {cr.Message.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
